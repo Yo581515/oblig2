b9f0dce7afaecdc1e0227f24213bf005a41de7a3
yfess <yfess@LAPTOP-6RK4P87O>
2021-09-29 10:21:12 +0200

    ferdig oppgve 3

--- Oblig2/src/Oppgave3Streams/Oppg3.java
+++ Oblig2/src/Oppgave3Streams/Oppg3.java
@@ -23,25 +23,18 @@ public class Oppg3 {
 
 //		3 a) 
 		System.out.println("a) Lag en ny liste som kun inneholder etternavnene til de ansatte. ");
-		List<String> etternavnene = ansatter.stream()
-				.map((a) -> a.getEtternavn())
-				.collect(Collectors.toList());
+		List<String> etternavnene = ansatter.stream().map((a) -> a.getEtternavn()).collect(Collectors.toList());
 		utskriftAlle(etternavnene);
 
 //		3 b) 
 		System.out.println("b) Finn ut antall kvinner blant de ansatte.");
-		int antallKvinner = (int) ansatter.stream()
-				.filter((a) -> a.getKjonn().equals(Kjonn.KVINNE))
-				.count();
+		int antallKvinner = (int) ansatter.stream().filter((a) -> a.getKjonn().equals(Kjonn.KVINNE)).count();
 		System.out.println(antallKvinner + "\n");
 
 //		3 c) 
 		System.out.println("c) Regn ut gjennomsnittslønnen til kvinnene.");
-		double gjennomsnittslønnen = ansatter.stream()
-				.filter(a -> a.getKjonn().equals(Kjonn.KVINNE))
-				.mapToDouble(a -> a.getAarslonn())
-				.average()
-				.orElse(Double.NaN);
+		double gjennomsnittslønnen = ansatter.stream().filter(a -> a.getKjonn().equals(Kjonn.KVINNE))
+				.mapToDouble(a -> a.getAarslonn()).average().orElse(0);
 		System.out.println(gjennomsnittslønnen + "\n");
 
 		// 3 d) Gi alle sjefer (stilling inneholder noe med "sjef") en lønnsøkning på 7%
@@ -49,12 +42,12 @@ public class Oppg3 {
 //		bruke streams direkte i stedet for metoden du laget i Oppg2. Skriv ut listen av 
 //		ansatte etter lønnsøkningen.
 //		double prosentTilegg = 07.0; // a.setAarslonn((int) (a.getAarslonn() * (1 + (prosentTilegg / 100))))
-//		ansatter = ansatter.stream()
+		ansatter.stream().filter(a -> a.getStilling().contains("sjef"))
+				.forEach(a -> a.setAarslonn((int) (a.getAarslonn() * 1.07)));
 
 //		3 e) 
 		System.out.println("e) Finn ut (true|false) om det er noen ansatte som tjener mer enn 800.000,-");
-		boolean ansattSomTjenerMerEnn800000 = ansatter.stream()
-				.anyMatch(a -> a.getAarslonn() > 800000);
+		boolean ansattSomTjenerMerEnn800000 = ansatter.stream().anyMatch(a -> a.getAarslonn() > 800000);
 		System.out.println(ansattSomTjenerMerEnn800000 + "\n");
 
 //		3 f) 
@@ -64,16 +57,14 @@ public class Oppg3 {
 
 //		3 g) 
 		System.out.println("g) Finn den/de ansatte som har lavest lønn.");
-		ansatter.stream()
-		.min(Comparator.comparing(Ansatt::getAarslonn))
-		.ifPresent(System.out::println);
-		System.out.println();
+		Optional<Ansatt> ansatt = ansatter.stream()
+		.min(Comparator.comparing(Ansatt::getAarslonn));
+//		.ifPresent(System.out::println);
+		System.out.println(ansatt);
 
 //		3 h) 
 		System.out.println("h) Finn ut summen av alle heltall i [1, 1000> som er delelig med 3 eller 5.");
-		int sum = IntStream
-				.range(1, 1000).filter(a -> a % 3 == 0 || a % 5 == 0)
-				.sum();
+		int sum = IntStream.range(1, 1000).filter(a -> a % 3 == 0 || a % 5 == 0).sum();
 		System.out.println(sum);
 
 	}
